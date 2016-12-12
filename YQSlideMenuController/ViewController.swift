d5684d3890745fbb9bb18e6df482063f5c4c3923
WangYQ <wyqpadding@gmail.com>
2016-12-12 21:42:14 +0800

    Initial Commit

---
+++ YQSlideMenuController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YQSlideMenuController
+//
+//  Created by Wang on 2016/12/12.
+//  Copyright © 2016年 Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
